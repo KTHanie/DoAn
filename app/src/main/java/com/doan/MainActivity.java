c5282a164bb2a8ea9ffdbee6c47c3a700958dacd
samg <lephat508@gmail.com>
2023-04-19 14:33:18 +0700

    phát

--- app/src/main/java/com/doan/MainActivity.java
+++ app/src/main/java/com/doan/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //sa mưa giông khùng
-        //sa mưa giông crazy
+
+        //hé lu bà
     }
 }
