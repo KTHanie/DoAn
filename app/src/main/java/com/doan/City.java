19556138a9934e7a1411b9b415733e8f74fb774b
samg <lephat508@gmail.com>
2023-04-26 15:54:56 +0700

    phat

---
+++ app/src/main/java/com/doan/City.java
@@ -0,0 +1,57 @@
+package com.doan;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class City {
+    String maTinh, tenTinh;
+    Bitmap hinhAnh;
+
+
+    public String getMaTinh() {
+        return maTinh;
+    }
+
+    public void setMaTinh(String maTinh) {
+        this.maTinh = maTinh;
+    }
+
+    public String getTenTinh() {
+        return tenTinh;
+    }
+
+    public void setTenTinh(String tenTinh) {
+        this.tenTinh = tenTinh;
+    }
+
+    public Bitmap getHinhAnh() {
+        return hinhAnh;
+    }
+
+    public void setHinhAnh(Bitmap hinhAnh) {
+        this.hinhAnh = hinhAnh;
+    }
+
+    public City(String maTinh, String tenTinh, Bitmap hinhAnh) {
+        this.maTinh = maTinh;
+        this.tenTinh = tenTinh;
+        this.hinhAnh = hinhAnh;
+    }
+
+    public static Bitmap convertStringToBitmapFromAccess (Context context, String filename) {
+        AssetManager assetManager = context.getAssets();
+        try {
+            InputStream is = assetManager.open(filename);
+            Bitmap bitmap = BitmapFactory.decodeStream(is);
+            return bitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- app/src/main/java/com/doan/MainActivity.java
+++ app/src/main/java/com/doan/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -36,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_Navigation, R.string.close_Navigation);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
         if (savedInstanceState == null) {
             navigationView.setCheckedItem(R.id.nav_home);
         }
